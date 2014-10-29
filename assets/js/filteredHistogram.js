760784a6d60e0f793bd5a788934f906b872cf210
mforssen <mforssen@hmc.edu>
2014-10-28 22:12:20 -0700

    parsing json file

--- assets/js/filteredHistogram.js
+++ assets/js/filteredHistogram.js
@@ -1,35 +1,50 @@
+var values = [];
+var maxPreemp = 0;
 
-var values = [0,0,0,1,2,2,3,3,4,4,4,4,5,5,6,1,8,0,0,0,0,0,0,0,0,0,1];
+  d3.json("test.json", function (data) {
 
-var value = crossfilter(values),
-  typeDimension = value.dimension(function(d) {return d;}),
-  typeGroup = typeDimension.group().reduceCount();
+    for (var i=0; i<data.tasks.length; i++)
+    {
+        if (data.tasks[i].name !== "<idle>") {
+          values.push(data.tasks[i]);
+          if (data.tasks[i].preemptionCount > maxPreemp)
+          {
+            maxPreemp = data.tasks[i].preemptionCount;
+          }
+        }
+    }
 
-
-var dataTable = dc.dataTable("#process-list");
-var histogram = dc.barChart("#dc-bar-chart");
-
-dataTable
-  .width(300)
-  .height(500)
-  .dimension(typeDimension)
-  .group(function(d) { return "List of all selected processes"})
-  .size(10000)
-    .columns([
-      function(d) {return "process name"},
-      function(d) {return d;}
-      ])
-    .sortBy(function(d) {return d;})
-    .order(d3.ascending);
-
-histogram
-  .width(500)
-  .height(400)
-  .x(d3.scale.linear().domain([0,10]))
-  .brushOn(true)
-  .dimension(typeDimension)
-  .group(typeGroup);
+    var value = crossfilter(values),
+    typeDimension = value.dimension(function(d) {return d.preemptionCount;}),
+    typeGroup = typeDimension.group().reduceCount();
 
 
-dc.renderAll();
+    var dataTable = dc.dataTable("#process-list");
+    var histogram = dc.barChart("#dc-bar-chart");
+
+    dataTable
+      .width(300)
+      .height(500)
+      .dimension(typeDimension)
+      .group(function(d) { return "List of all selected processes"})
+      .size(10000)
+        .columns([
+          function(d) {return d.name},
+          function(d) {return d.preemptionCount;}
+          ])
+        .sortBy(function(d) {return d.preemptionCount;})
+        .order(d3.ascending);
+
+    histogram
+      .width(500)
+      .height(400)
+      .x(d3.scale.linear().domain([0,maxPreemp + 1]))
+      .brushOn(true)
+      .dimension(typeDimension)
+      .group(typeGroup);
+
+
+    dc.renderAll();
+
+    });
 
