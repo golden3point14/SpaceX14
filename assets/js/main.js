d4385e6221ad499b03e7eeac9813d23215cbf502
Wendy Brooks <wendybrooks314@gmail.com>
2014-11-08 14:19:43 -0800

    main page displays top 10 preempted

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,8 @@ function handleFileSelect(evt) {
 			var JSONObj = obj;
 			JSONtasks = obj.tasks;
 			getTopPreemptions();
+      getTopRunTime();
+      GetTopWaitTime();
 
 		};
 
@@ -46,18 +48,55 @@ document.getElementById('files').addEventListener('change', handleFileSelect, fa
 
 function getTopPreemptions()
 {
-	//sort processes by preemptionCount
- 	preemptionSorted = _.sortBy(JSONtasks, function(element){return element.preemptionCount;});
- 	//remove <idle>
- 	preemptionSorted = _.select(preemptionSorted, function(element){return element.name != "<idle>";});
- 	for (var i=0; i<preemptionSorted.length; i++)
- 	{
- 		console.log(preemptionSorted[i].name + " " + preemptionSorted[i].preemptionCount);
- 	}
- 	//sort processes by totalRunTime
- 	//sort processes by totalWaitTime
 
- 	//get top 10 most preempted (ignore idle)
- 	//get top 10 longest runtime (ignore idle)
- 	//get top 10 longest waittime (ignore idle)
+  //JSONtasks = _.select(JSONtasks, function(element){return element.name != "<idle>";});
+
+	//sort processes by preemptionCount
+ 	preemptionSorted = _.sortBy(JSONtasks, function(element){return -1*(element.preemptionCount);});
+ 	//remove <idle>
+ 	preemptionSorted = _.select(preemptionSorted, function(element){return (element.name != "<idle>") && (element.preemptionCount != 0);});
+ 	
+  var displayNum = 10;
+  var display = [];
+  if (preemptionSorted.length < displayNum)
+  {
+    display = preemptionSorted;
+  }
+  else
+  {
+    display = preemptionSorted.slice(0,displayNum);
+  }
+
+  var value = crossfilter(display),
+    typeDimension = value.dimension(function(d) {return d.preemptionCount;}),
+    nameDimension = value.dimension(function(d) {return d.pid;}),
+    nameGroup = nameDimension.group().reduceSum(function(d) {return 5}),
+    typeGroup = typeDimension.group().reduceCount();
+
+  var dataTable = dc.dataTable("#preemption-list");
+    
+  dataTable
+    .width(300)
+    .height(400)
+    .dimension(typeDimension)
+    .group(function(d) { return "top 10 most preempted"})
+    .columns([
+      function(d) {return d.name;},
+      function(d) {return d.pid;},
+      function(d) {return d.preemptionCount;}
+      ])
+    .sortBy(function(d) {return -1*d.preemptionCount;})
+    .order(d3.ascending);
+
+  dc.renderAll();
+}
+
+function getTopRunTime()
+{
+
+}
+
+function getTopWaitTime()
+{
+
 }
