074409d6a2e86b81a56d3a0a74b09547c4e6aae6
mforssen <mforssen@hmc.edu>
2015-04-12 14:55:18 -0700

    removing unused code

--- assets/js/preemptionChart.js
+++ assets/js/preemptionChart.js
@@ -59,7 +59,6 @@ function openDB()
     var objectStore = xact.objectStore("Tasks");
     var ob = objectStore.get(1); //temporary hard-coded
     ob.onsuccess = function(e) { console.log("e is the JSONtasks");
-                                 // console.log(e.target.result);
                                  JSONtasks = e.target.result;
                                  useDatabaseData();
                                }
--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,6 @@
 var sidebar = document.getElementById('sidebar');
 
+// Overview page
 var mainDiv = document.createElement('div');
 mainDiv.id = 'mainButton';
 mainDiv.className = 'sidebutton';
@@ -7,6 +8,7 @@ mainDiv.innerHTML = "Overview";
 mainDiv.setAttribute("onclick", "window.location.href='main.html';");
 sidebar.appendChild(mainDiv);
 
+// Cycles page
 var cyclesDiv = document.createElement('div');
 cyclesDiv.id = 'cyclesButton';
 cyclesDiv.className = 'sidebutton';
@@ -14,6 +16,7 @@ cyclesDiv.innerHTML = "Cycles";
 cyclesDiv.setAttribute("onclick", "window.location.href='cycles.html';");
 sidebar.appendChild(cyclesDiv);
 
+// Task Statistics page 
 var statisticsDiv = document.createElement('div');
 statisticsDiv.id = 'statisticsButton';
 statisticsDiv.className = 'sidebutton';
@@ -21,21 +24,7 @@ statisticsDiv.innerHTML = "Task Statistics";
 statisticsDiv.setAttribute("onclick", "window.location.href='histogram.html';");
 sidebar.appendChild(statisticsDiv);
 
-// var statsDiv = document.createElement('div');
-// statsDiv.id = 'sidebutton';
-// statsDiv.className = 'sidebutton';
-// statsDiv.innerHTML = "Distributions";
-// sidebar.appendChild(statsDiv);
-
-/*
-var searchDiv = document.createElement('div');
-searchDiv.id = 'searchButton';
-searchDiv.className = 'sidebutton';
-searchDiv.innerHTML = "Search";
-searchDiv.setAttribute("onclick", "window.location.href='search.html';");
-sidebar.appendChild(searchDiv);
-*/
-
+// Task State page
 var processDiv = document.createElement('div');
 processDiv.id = 'processButton';
 processDiv.className = 'sidebutton';
@@ -43,6 +32,7 @@ processDiv.innerHTML = "Task State";
 processDiv.setAttribute("onclick", "window.location.href='process.html';");
 sidebar.appendChild(processDiv);
 
+// Compare Tasks page
 var compareDiv = document.createElement('div');
 compareDiv.id = 'compareButton';
 compareDiv.className = 'sidebutton';
